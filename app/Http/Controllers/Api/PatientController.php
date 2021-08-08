0a42f4a0b0c11ba7676dda669b3062a60ff5e148
ardi7923 <ardiirawan777@gmail.com>
2021-08-08 21:57:20 +0800

    patient with search done

--- app/Http/Controllers/Api/PatientController.php
+++ app/Http/Controllers/Api/PatientController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Patient;
+use Illuminate\Http\Request;
 
 class PatientController extends Controller {
 
-    public function index()
+    public function index(Request $request)
     {
-        return response()->json(["message" => "Data Pasien","data" => Patient::all()], 200);
+        $patient = Patient::query();
+
+        $q = $request->q;
+
+        if($request->q != null){
+            $patient->where("name","like","%".$q."%")
+                    ->orWhere("room","like","%".$q."%")
+                    ->orWhere("address","like","%".$q."%");
+        }
+
+
+        return response()->json(["message" => "Data Pasien","data" => $patient->get()], 200);
     }
 }
