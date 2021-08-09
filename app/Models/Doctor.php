7b759055a54d27f54c7f837168420c62058a44d3
ardi7923 <ardiirawan777@gmail.com>
2021-08-09 13:37:01 +0800

    get attribute image doctor done

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -10,4 +10,14 @@ class Doctor extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    protected $hidden = [
+        "created_at",
+        "updated_at"
+    ];
+
+    public function getImageAttribute()
+    {
+        return env("APP_URL")."/storage/images/".$this->attributes["image"];
+    }
 }
